6b82575729c8e7b91f744980e473d6eb822e6e7e
Ashley Camba Garrido <ashwoods@gmail.com>
2015-11-10 16:27:06 +0100

    initial pytest settings

---
+++ Makefile
@@ -0,0 +1,10 @@
+.PHONY: test release
+
+test:
+	py.test
+
+release:
+	python setup.py sdist bdist_wheel register upload -s
+
+flake8:
+	flake8 pg_returning tests
--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='django-update-returning',
+    name='django-pg-returning',
     version='0.3.1',
     author=u'kanu',
-    packages=['update_returning'],
-    url='http://github.com/kanu/django-update-returning',
+    packages=['pg_returning'],
+    url='http://github.com/ashwoods/django-pg-returning',
     license='Public Domain',
     description='A queryset that can return the changed rows of updates on a postgresql databases',
     long_description=open('README.rst').read(),
     zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Utilities',
+    ],
 )
