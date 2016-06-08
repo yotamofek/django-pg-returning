5605c91a9d581abf2880f80577a5fdf4e84e1eea
yotamofek <yotam.ofek@gmail.com>
2016-06-09 02:11:14 +0300

    Revert "initial pytest settings"
    This reverts commit 6b82575729c8e7b91f744980e473d6eb822e6e7e.

--- setup.py
+++ setup.py
@@ -1,26 +1,14 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from distutils.core import setup
 
 setup(
-    name='django-pg-returning',
+    name='django-update-returning',
     version='0.3.1',
     author=u'kanu',
-    packages=['pg_returning'],
-    url='http://github.com/ashwoods/django-pg-returning',
+    packages=['update_returning'],
+    url='http://github.com/kanu/django-update-returning',
     license='Public Domain',
     description='A queryset that can return the changed rows of updates on a postgresql databases',
     long_description=open('README.rst').read(),
     zip_safe=False,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Topic :: Utilities',
-    ],
 )
